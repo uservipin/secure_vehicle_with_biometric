03703ff2b5dbfc5021ba191a394b04e819c9d2bd
uservipin <vipin.15bme2066@abes.ac.in>
2018-07-06 04:12:06 +0530

    addition of saprk plug in project and work properly in proteus

--- vipin_test_locker/vipin_test_locker.ino
+++ vipin_test_locker/vipin_test_locker.ino
@@ -8,17 +8,16 @@
 //open bluetooth module
 //send data to initialize relay to power the finger print
 
-
 #include <math.h>
 #include<stdio.h>
-
 int input_read = 2;
+int pass_circuit = 6;
 int output_data = 7;
 int output_relay_two = 12;
 int output_relay_four = 11;
 int data = 0;
 String password;
-String vipin = "VIPIN";
+//String vipin = "VIPIN";
 int count = 0;
 int count_again = 0;
 
@@ -31,21 +30,21 @@ struct test {
 
 struct test function() {
   struct test t;
-  Serial.println("welcome in struct function ");
-  delay(1000);
+  //  Serial.println("welcome in struct function ");
+  delay(150);
   data = digitalRead(input_read);
   //t.counter ;
   //  Serial.println("initial value of counter in struct function is");
   //  Serial.println(t.counter);
 
   t.d_input = data;
-  Serial.println("print the value of t.d_input in struct  function");
-  Serial.println(t.d_input);
+  //  Serial.println("print the value of t.d_input in struct  function");
+  //  Serial.println(t.d_input);
 
   if (t.d_input == 1) {
     count = count + 1;
-    Serial.print("value of count in struct function is :  "); Serial.println(count);
-    Serial.println("end of struct function ");
+    //    Serial.print("value of count in struct function is :  "); Serial.println(count);
+    //    Serial.println("end of struct function ");
   }
   return t;
 }
@@ -54,29 +53,35 @@ struct test function() {
 //............................................................................................................stop_while_bluetooth
 
 void stop_while_bluetooth() {
-  Serial.println("stop while bluetooth is called");
+  //  Serial.println("stop while bluetooth is called");
   loop();
 }
 
 // .............................................................................................................. key insert
 void key_insert() {
-  Serial.println("key_insert function is called ");
-  digitalWrite(output_relay_four, LOW);    // turn the LED off by making the voltage LOW
-  digitalWrite(output_relay_two, LOW);    // turn the LED off by making the voltage LOW
+  //  Serial.println("key_insert function is called ");
+
+
+
+  //......................................................................................................................why
+  //                                                                                                                     ..
+  digitalWrite(output_relay_four, LOW);    // turn the LED off by making the voltage LOW                               ..
+  digitalWrite(output_relay_two, LOW);    // turn the LED off by making the voltage LOW                                ..
+  //  .....................................................................................................................
   data = digitalRead(input_read);
-  Serial.print("print digital read inside key_insert data is : ");   Serial.println(data);
-  delay(1000);
+  //  Serial.print("print digital read inside key_insert data is : ");   Serial.println(data);
+  delay(150);
 
   if (data == 1) {
     digitalWrite(output_data, HIGH);
-    Serial.println("key is on and power supply is ON for other function ");
-    delay(1000);
+    Serial.println("key is ON and power supply is ON");
+    delay(150);
   }
 
   if (data == 0) {
     digitalWrite(output_data, LOW );
-    Serial.println("  key is off and power supply is OFF  for other function");
-    delay(1000);
+    Serial.println("  key is OFF and power supply is OFF  ");
+    delay(150);
   }
 }
 
@@ -84,18 +89,21 @@ void key_insert() {
 int bike_on_function() {
 
   while (1) {
-    digitalWrite(output_data, LOW);
-    Serial.println("count is 2 and data is 1");
+
+    //    Serial.println("count is 2 and data is 1");
     Serial.println("  bike ONN...");
+    digitalWrite(output_data, HIGH);
+    delay(2000);
     int data = digitalRead(input_read);
+
     if (data == 1) {
-      Serial.println("inside bike  ON  while loop function  in if statment : .. ");
+      //      Serial.println("inside bike  ON  while loop function  in if statment : .. ");
       count_again = count_again + 1;
-      Serial.println(count_again);
+      //      Serial.println(count_again);
       return count_again;
     }
   }
-  delay(1000);
+  delay(150);
 }
 
 //...........................................................................................................bike function off
@@ -103,14 +111,14 @@ int bike_on_function() {
 int bike_function_off () {
   while (1) {
     digitalWrite(output_data, LOW );
-    Serial.println("count is 2 and data is 0");
+    //    Serial.println("count is 2 and data is 0");
     Serial.println("bike off");
 
     int data = digitalRead(input_read);
     if (data == 1) {
-      Serial.println("inside bike OFF  function IN while  in if statment : .. ");
+      //      Serial.println("inside bike OFF  function IN while  in if statment : .. ");
       count_again = 0;
-      delay(1000);
+      delay(150);
       return count_again;
     }
   }
@@ -123,8 +131,8 @@ void bluetooth() {
   digitalWrite(output_relay_four, HIGH);   // turn the LED on (HIGH is the voltage level)
   digitalWrite(output_relay_two, HIGH);    // turn the LED on (HIGH is the voltage level)
   data = digitalRead(input_read);
-  Serial.print("print digital read inside bluetooth  data is : "); Serial.println(data);
-  delay(1000);
+  //  Serial.print("print digital read inside bluetooth  data is : "); Serial.println(data);
+  delay(150);
 
   //  Serial.println("now flow goes to structure function  : ");
   //  delay(1000);
@@ -133,8 +141,8 @@ void bluetooth() {
   //  Serial.println(test_call.counter);
   //  Serial.println(test_call.d_input);
 
-  Serial.println("press the push button ");
-  delay(1000);
+  //  Serial.println("press the push button ");
+  //  delay(150);
 
   if (data == 1) {
     count_again = count_again + 1;
@@ -167,29 +175,47 @@ String  string_read() {
 
 //.................................................................................................correct_code
 
-int correct_code(){
+int correct_code() {
   while (1) {
-          bluetooth();
-        String   read_string = string_read();
-          if (read_string == "STOP") {
-            stop_while_bluetooth();
-            return 1;
-          }
-          delay(1000);
-        }
-
-
-  
+    bluetooth();
+    String   read_string = string_read();
+    if (read_string == "STOP") {
+      stop_while_bluetooth();
+      return 1;
+    }
+    delay(1000);
+  }
 }
 
-//.................................................................................................. setup 
+
+void bypass_bluetooth() {
+  Serial.print("inside pass function");
+  digitalWrite(pass_circuit, HIGH);
+  digitalWrite(output_relay_two, HIGH);
+  digitalWrite(output_relay_four, HIGH);
+  data = digitalRead(input_read);
+  //  Serial.print("print digital read inside key_insert data is : ");   Serial.println(data);
+  delay(150);
+
+  if (data == 1) {
+    digitalWrite(output_data, HIGH);
+    Serial.println("key is ON and power supply is ON");
+    delay(150);
+  }
+  delay(5000);
+  digitalWrite(pass_circuit, LOW);
+
+}
+
+//.................................................................................................. setup
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  Serial.println("wELCOME IN SECCURITY SYSTEM SOLUTIONS :");
-  Serial.println(data);
+  Serial.println("wELCOME IN SECCURITY SYSTEM SOLUTIONS... :");
+  //  Serial.println(data);
   pinMode(output_relay_four, OUTPUT);
+  pinMode(pass_circuit, OUTPUT);
   pinMode(output_relay_two, OUTPUT);
   pinMode(input_read, INPUT);
   pinMode(output_data, OUTPUT);
@@ -200,33 +226,33 @@ void setup() {
 
 
 void loop() {
-//  Serial.println("LOOP.....LOOP.....LOOP.....");
+  //  Serial.println("LOOP.....LOOP.....LOOP.....");
   int check_data = 0;
   while (check_data == 0) {
     if (!Serial.available()) {
+      Serial.println("inside not serial");
       key_insert();
-//      delay(1000);
+      //      delay(1000);
     }
-
-    Serial.println("inside check data while loop ");
-//    delay(1000);
+    //    Serial.println("WELCOME IN SECURITY LOOP ........ ");
+    //    delay(1000);
     if ( Serial.available() ) {
       String  read_string = string_read();
       if (read_string != "VIPIN" && read_string != "STOP") {
         serial_read();
       }
-
+      if (read_string == "PASS") {
+        bypass_bluetooth() ;
+      }
       if (read_string == "VIPIN") {
         Serial.println("your password is correct YOU HAVE LOGIN :...");
-      int correct_code_variable = correct_code();
-      
+        int correct_code_variable = correct_code();
       }
       //      break;
     }
     check_data = check_data + 1;
-//    Serial.print("value of check data value is:   "); Serial.println(check_data);
+    //    Serial.print("value of check data value is:   "); Serial.println(check_data);
   }
-
 }
 
 
